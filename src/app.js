5aba309cf85d2785430680e16476984fc46eeb30
SKM-MAC <social@saadkhanmalik.com>
2016-09-16 11:21:19 +0300

    Using Templates with Express(Finished Stage)

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ var express = require('express'),
 var app = express();
 
 app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates');
+app.set('views', __dirname + '/templates')
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index')
 });
 
-app.get('/blog/:title?', function(req, res){ 
+app.get('/blog/:title?', function(req, res){
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
 		res.send("This page is under construction!");
 	} else {
 		var post = posts[title] || {};
-		res.render('post', { post: post});
+		res.render('post', {post: post});
 	}
 });
 
